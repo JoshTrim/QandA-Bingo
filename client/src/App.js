3c43ca9a0e8cabce36bf7cac4fa0ad927d652b0e
Josh Trim <josh.trim@tuta.io>
2024-01-27 16:45:19 -0800

    made video player component. Looks like embedding of QandA vids is disabled serverside - will have to think about a workaround.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import { Chat } from './Chat';
-
+import Player from './video/Player'
 
 
 function App() {
   return (
     <>
       <h1>Socket.io App</h1>
+      <Player url="https://www.youtube.com/embed/SoRCmTQxzEk?controls=0"/>
       <Chat />
     </>
   )
--- server/run.py
+++ server/run.py
@@ -25,4 +25,4 @@ async def home():
     return {"message" : "Hello world!"}
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run('run:app', reload=True)
